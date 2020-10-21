42ba1bc05c8658e778b2ba13d82a2b6dd54ef4da
Quốc Vũ <nguyenquocvu.work@gmail.com>
2020-10-21 19:50:21 +0700

    Init Navbar

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import CssBaseLine from "@material-ui/core/CssBaseline";
+import NavBar from "./NavBar";
+function Home() {
+  return (
+    <div>
+      <CssBaseLine />
+      <NavBar />
+    </div>
+  );
+}
+export default Home;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+function NavBar() {
+  return (
+    <div>
+      <BottomNavigation>
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
+    </div>
+  );
+}
+export default NavBar;
